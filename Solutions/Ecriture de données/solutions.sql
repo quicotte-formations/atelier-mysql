3cd8a9c40b3a64080d349520f3c21509a285112f
quicotte-formations <quicotte@gmail.com>
2026-03-25 16:06:20 +0100

    Ex 4

--- Solutions/Ecriture de données/solutions.sql
+++ Solutions/Ecriture de données/solutions.sql
@@ -4,3 +4,11 @@ VALUES (2,6,'2023/12/05','2023/12/12', 980);
 UPDATE  chambre
 SET     prix = 200
 WHERE   id = 6;
+
+UPDATE chambre 
+SET prix = (    SELECT avg_prix 
+                FROM ( 
+                    SELECT  AVG(prix) AS avg_prix 
+                    FROM chambre 
+                    WHERE type_chambre = 'Chambre Double') AS temp) 
+WHERE type_chambre = 'Chambre Simple';
